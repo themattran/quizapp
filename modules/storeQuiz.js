eff6c85e869b64191847854bc45b541855873e87
Johannes Weber <johannes@drweber.de>
2022-02-05 10:36:45 -0800

    clean up storeQuiz

--- modules/storeQuiz.js
+++ modules/storeQuiz.js
@@ -1,94 +1,51 @@
 /*
   Module to handle storing and retrieving of quizzes.
 
-  Exports two functions:
+  Takes a single quiz object from the POST route handler which
+  contains all the data (questions and options) of the new quiz,
+  then stores the data into the respective database tables
+  and returns just the new record from the quiz table
 
-  ** retrieveQuiz: ******************************************************
-        Retrieves a single quiz with all its data (questions and options)
-        from the database, assembles it into a JSON object, and returns
-        that object to the caller (which will then send it to back to the browser)
+  Structure of received object:
+  (id's are undefined because records don't exist yet)
 
-        Structure of returned quiz object:
-
+  {
+    id: undefined,
+    name: string,
+    questions: [
+      {
+        id: undefined
+        question_string: string
+        quiz_id: undefined
+        options: [
           {
-            id: integer,
-            name: string,
-            questions: [
-              {
-                id: integer
-                question_string: string
-                quiz_id: integer
-                options: [
-                  {
-                    id: integer,
-                    question_id: integer,
-                    content: text,
-                    is_correct: bool
-                  },
-                  ...
-                ]
-              },
-              ...
-            ]
-          }
-
-  ** storeQuiz: ******************************************************
-        Takes a single quiz object from the POST route handler which
-        contains all the data (questions and options) of the new quiz,
-        then stores the data into the respective database tables
-        and returns just the new record from the quiz table
-
-        Structure of received object:
-        (id's are undefined because records don't exist yet)
-
-        {
-          id: undefined,
-          name: string,
-          questions: [
-            {
-              id: undefined
-              question_string: string
-              quiz_id: undefined
-              options: [
-                {
-                  id: undefined,
-                  question_id: undefined,
-                  content: text,
-                  is_correct: bool
-                },
-                ...
-              ]
-            },
-            ...
-          ]
-        }
+            id: undefined,
+            question_id: undefined,
+            content: text,
+            is_correct: bool
+          },
+          ...
+        ]
+      },
+      ...
+    ]
+  }
 
 */
 
-
-
 /**
- * Retrieves a single quiz with its questions and options
+ * Takes a quiz object and stores the quiz record only (not questions or options) to the db
  * @param {object} db The database driver reference
- * @param {integer} quiz_id The id of the quiz to retrieve
- * @returns a promise to an object containing the quiz data
+ * @param {integer} userId The id of the user who owns the quiz
+ * @param {object} quiz The quiz object to store, containing questions and options data
+ * @returns a promise to the completed task of storing the quiz record
  */
-const retrieveQuiz = (db, quizId) => {
-  //Matt
-};
-
-
-
-/* Functions related to storeQuiz below ********************************************************/
-
-const storeQuizRecord = (db, user_id, quiz) => {
+const storeQuizRecord = (db, userId, quiz) => {
   return new Promise((resolve, reject) => {
-
     const query = {
       text: 'INSERT INTO quizzes (name, user_id) VALUES ($1, $2) RETURNING *;',
-      values: [ quiz.name, user_id ]
-    }
-
+      values: [ quiz.name, userId ]
+    };
     db.query(query)
       .then(data => {
         const quizRecord = data.rows[0];
@@ -97,18 +54,22 @@ const storeQuizRecord = (db, user_id, quiz) => {
       .catch(err => {
         reject(err);
       });
-
   });
 };
 
-
+/**
+ * Takes a (single) question object and stores its data to the db (just the question, not its options)
+ * @param {object} db The database driver reference
+ * @param {integer} quizId The id of the quiz the question belongs to
+ * @param {object} question The question object
+ * @returns a promise to the completed task of storing the question record
+ */
 const storeQuestionRecord = (db, quizId, question) => {
   return new Promise((resolve, reject) => {
-
     const query = {
       text: 'INSERT INTO questions (quiz_id, question_string) VALUES ($1, $2) RETURNING *;',
       values: [ quizId, question.question_string ]
-    }
+    };
     db.query(query)
       .then(data => {
         const questionRecord = data.rows[0];
@@ -117,18 +78,23 @@ const storeQuestionRecord = (db, quizId, question) => {
       .catch(err => {
         reject(err);
       });
-
   });
 };
 
 
+/**
+ * Takes a (single) option object and stores its data to the db
+ * @param {object} db The database driver reference
+ * @param {integer} questionId The id of the question to store the option for
+ * @param {object} option The option object to store
+ * @returns a promise to the completed task of storing the option
+ */
 const storeOptionRecord = (db, questionId, option) => {
   return new Promise((resolve, reject) => {
-
     const query = {
       text: 'INSERT INTO options (question_id, content, is_correct) VALUES ($1, $2, $3) RETURNING *;',
       values: [ questionId, option.content, option.is_correct ]
-    }
+    };
     db.query(query)
       .then(data => {
         const optionRecord = data.rows[0];
@@ -137,72 +103,86 @@ const storeOptionRecord = (db, questionId, option) => {
       .catch(err => {
         reject(err);
       });
-
   });
 };
 
+/**
+ * Takes a (single) question object and a quizId and stores the question and its options in the db
+ * @param {object} db The database driver reference
+ * @param {integer} quizId The id of the quiz to store the question data for
+ * @param {object} question The question object to store
+ * @returns a promise to the completed task of storing the question with its options
+ */
 const storeQuestion = (db, quizId, question) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     storeQuestionRecord(db, quizId, question)
       .then(questionRecord => {
-        console.log("Stored Question Record for: ", questionRecord.question_string);
         const promises = [];
-
-        for (option of question.options) {
+        for (const option of question.options) {
           promises.push(storeOptionRecord(db, questionRecord.id, option));
         }
-
         Promise.all(promises).then(() => {
-          console.log(`All option for question ${questionRecord.id} stored.`);
+          resolve();
+        }).catch(err => {
+          reject(err);
         });
-
       });
   });
-}
+};
 
+/**
+ * Takes an array of questions and a quizId, and stores the questions/options for the quiz
+ * @param {object} db The database driver reference
+ * @param {integer} quizId The id of the quiz to store questions for
+ * @param {array} questions The questions to be stored
+ * @returns a Promise to the completed task of storing the questions
+ */
 const storeQuestions = (db, quizId, questions) => {
-  return new Promise ((resolve, reject) => {
-
+  return new Promise((resolve, reject) => {
     const promises = [];
-
-    for (question of questions) {
+    for (const question of questions) {
       promises.push(storeQuestion(db, quizId, question));
-    };
-
-    Promise.all(promises).then(values => {
-      console.log("Stored all question records", values);
+    }
+    Promise.all(promises).then(() => {
       resolve();
+    }).catch(err => {
+      reject(err);
     });
-
   });
 };
+
 /**
  * Takes a quiz object with questions and options and stores it to the database
  * @param {object} db The database driver reference
- * @param {object} user_id The user_id of the quiz owner
+ * @param {integer} user_id The user_id of the quiz owner
  * @param {object} quiz The object with the quiz data
  * @returns a promise to newly created quiz record only (not questions and options)
  */
 const storeQuiz = (db, userId, quiz) => {
   //Johannes
   /*
-    generate quiz record and retrieve id
+    Approach
+
+    store quiz record and retrieve id
     for each question {
-      generate question record and retrieve id
+      store question record, retrieve id
+    }
+    for each question record {
       for each option {
-        generate option record
+        store option record
       }
     }
   */
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     storeQuizRecord(db, userId, quiz)
       .then(quizRecord => {
-        console.log("Stored Quiz Record for:", quizRecord.name);
-        storeQuestions(db, quizRecord.id, quiz.questions);
+        return storeQuestions(db, quizRecord.id, quiz.questions);
       })
       .then(questionRecords => {
-        console.log("Stored all questions");
+        console.log("Stored quiz successfully");
         resolve();
+      }).catch(err => {
+        reject(err);
       });
   });
 
