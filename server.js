214ae7fe6e6def07de108f26ede3fc46f6b89878
Johannes Weber <johannes@drweber.de>
2022-02-08 21:10:24 -0800

    add express-session, use auth routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const session = require("express-session");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -18,7 +19,11 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(session({
+  secret: 'the secret',
+  resave: false,
+  saveUninitialized: true
+}));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -46,7 +51,10 @@ app.use("/api/users", usersRoutes(db));
 app.use("/api/quizzes", quizzesRoutes(db));
 app.use("/api/questions", questionsRoutes(db));
 app.use("/api/options", optionsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes(db));
 
 // Home page
 // Warning: avoid creating more routes in this file!
