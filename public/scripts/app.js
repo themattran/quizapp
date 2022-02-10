f9505936cf967155f934f5b8ef775fbd15d3ff94
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-10 06:06:53 +0000

    fix jquery handler to handle dynamic elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,41 +2,43 @@
 
 const additionalOption = 
   `<div class="modular-option-node">
-    <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
-      <fieldset>
-      <input type="checkbox" id="correct" name="correct">
-      <span>Correct?</span>
-     </fieldset>
-   </div>`;
+  <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
+  <fieldset>
+    <input type="checkbox" id="correct" name="correct">
+    <span>Correct?</span>
+ </div>
+   `;
 
 const additionalQuestion = 
-`<article id="question-option-container" margin-top:10px>
+`<article id="question-option-container">
 <textarea id="new-quiz-question" name="question" placeholder="Enter your question" required=""></textarea>
-<div class="options-container">
+<div id="options-parent">
+  <div id="options-target">
   <div class="modular-option-node">
 <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
 <fieldset>
   <input type="checkbox" id="correct" name="correct">
   <span>Correct?</span>
-</fieldset>
 </div>
 </div>
-<button class="nother-question">+</button>
+<button class="nother-option">+</button>
+</div>
 <div class="button-flex">
 <button class="save-button">+ Add next question</button>
 </div>
 </article>`;
 
 $(document).ready(function() { 
-  $(".nother-question").click(function(e) {
+  
+  $(document).on('click', '.nother-option', function(e) {
     e.preventDefault();
-    $(".options-container").append(additionalOption);
+    $(this).parent().find("#options-target").append(additionalOption);
   })
 
-  $(".save-button").click(function(e) {
+  $(document).on('click', '.save-button', function(e) {
     e.preventDefault();
     $(this).hide();
-    $(".questions-container").append(additionalQuestion);
+    $(this).closest(".questions-container").append(additionalQuestion);
   })
 });
 
