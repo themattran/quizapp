53ffe458053b74882d74238a469d70176dc7344e
Johannes Weber <johannes@drweber.de>
2022-02-10 14:11:54 -0800

    add parseQuiz() function and submit functionality

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -1,15 +1,15 @@
 //Element representing additional option
-const additionalOption = 
+const additionalOption =
   `<div class="modular-option-node">
-  <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
-  <fieldset>
-    <input type="checkbox" id="correct" name="correct">
-    <span>Correct?</span>
- </div>
+    <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
+    <fieldset>
+      <input type="checkbox" id="correct" name="correct">
+      <span>Correct?</span>
+   </div>
    `;
 
 //Element representing additional question
-const additionalQuestion = 
+const additionalQuestion =
 `<article id="question-option-container">
 <textarea id="new-quiz-question" name="question" placeholder="Enter your question" required=""></textarea>
 <div id="options-parent">
@@ -28,8 +28,58 @@ const additionalQuestion =
 </div>
 </article>`;
 
-$(document).ready(function() { 
-  
+/**
+ * Parse quiz data on form and return quiz object
+ */
+const parseQuiz = () => {
+  const quizObject = {};
+
+  //Find the quiz title
+  const quizTitle = $("#new-quiz-title").val();
+  quizObject.name = quizTitle;
+  console.log("Begin parsing quiz: ", quizTitle);
+
+  //Find the elements containing question data
+  const questionContainers = $.find(".questions-container > article");
+
+  quizObject.questions = [];
+
+  //Loop through the question elements and assemble question data in turn
+  for (const qCont of questionContainers) {
+    //Find the question string for this question
+    const qTextArea = $(qCont).find("> textarea")[0];
+    const questionString = $(qTextArea).val();
+    console.log("- Parsing question: ", questionString);
+
+    const questionObject = {};
+    questionObject.question_string = questionString;
+    questionObject.options = [];
+
+    //Find the elements containing the option data
+    const questionOptionContainers = $(qCont).find(".modular-option-node");
+
+    //Loop through options and build options array for this question
+    for (const qoCont of questionOptionContainers) {
+      const optionContent = $(qoCont).find("textarea").val();
+      const optionCorrect = $(qoCont).find("input")[0].checked;
+      console.log("  - Parsing option: ", optionContent, optionCorrect);
+
+      const optionObject = {
+        content: optionContent,
+        is_correct: optionCorrect,
+      };
+      questionObject.options.push(optionObject);
+    }
+
+    quizObject.questions.push(questionObject);
+  }
+
+  console.log(`Finished parsing quiz with ${quizObject.questions.length} questions`);
+  return quizObject;
+};
+
+$(document).ready(function() {
+
   $(document).on('click', '.nother-option', function(e) {
     e.preventDefault();
     $(this).parent().find("#options-target").append(additionalOption);
@@ -40,4 +90,17 @@ $(document).ready(function() {
     $(this).hide();
     $(this).closest(".questions-container").append(additionalQuestion);
   })
+
+
+  /**
+   * Event handler for the submit quiz button: parse quiz and post JSON object to /api/quizzes
+   */
+  $(".submit-quiz-object").click(function(e) {
+    e.preventDefault();
+    const quizObject = parseQuiz();
+    $.post("/api/quizzes", quizObject)
+      .done(data => {
+        console.log(data);
+      });
+  });
 });
