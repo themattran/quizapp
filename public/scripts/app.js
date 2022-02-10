83359359f4f8a1fa2998006662c3c90ec387dc56
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-10 03:38:35 +0000

    add button functionality to create new question article elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 // Client facing scripts here
 
-let additionalOption = 
+const additionalOption = 
   `<div class="modular-option-node">
     <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
       <fieldset>
@@ -9,10 +9,34 @@ let additionalOption =
      </fieldset>
    </div>`;
 
+const additionalQuestion = 
+`<article id="question-option-container" margin-top:10px>
+<textarea id="new-quiz-question" name="question" placeholder="Enter your question" required=""></textarea>
+<div class="options-container">
+  <div class="modular-option-node">
+<textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
+<fieldset>
+  <input type="checkbox" id="correct" name="correct">
+  <span>Correct?</span>
+</fieldset>
+</div>
+</div>
+<button class="nother-question">+</button>
+<div class="button-flex">
+<button class="save-button">+ Add next question</button>
+</div>
+</article>`;
+
 $(document).ready(function() { 
   $(".nother-question").click(function(e) {
     e.preventDefault();
     $(".options-container").append(additionalOption);
+  })
+
+  $(".save-button").click(function(e) {
+    e.preventDefault();
+    $(this).hide();
+    $(".questions-container").append(additionalQuestion);
   })
 });
 
