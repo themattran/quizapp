ff1ffde81632323f4bfd96edb9e25d14099c61c6
Matthew Tran <m.tran906@gmail.com>
2022-02-10 19:33:00 +0000

    make changes to css file and make quizzes dynamic using jquery

--- public/scripts/list-quizzes.js
+++ public/scripts/list-quizzes.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
   $("#add-quiz").click(function() {
-    alert("hello");
+    $.ajax({
+      method: "GET",
+      url: "/api/createQuizzeesssssss"
+    }).done((res) => {
+      console.log('quizzes', res);
+      renderQuizzes(res.quizzes.slice(0, 4));
+    });
   })
   $.ajax({
         method: "GET",
         url: "/api/quizzes"
       }).done((res) => {
         console.log('quizzes', res);
-        for(quiz of res.quizzes) {
-         $("<div>").text(quiz.name).appendTo($("body"));
-      }
-   });;
-
-   
+        renderQuizzes(res.quizzes.slice(0, 4));
+   });
 
 });
+
+
+const renderQuizzes = function(quizzes) {
+  // $("#list-quizzes").empty();
+  for (const quiz of quizzes) {
+      $('#list-quizzes').append(quizCard(quiz.name));
+  };
+}
+
+//load quizzes backwards
+
+const quizCard = (name) => {
+  console.log('name', name);
+  return $(`
+<div class="quiz-card">
+        <i class="fa-duotone fa-gun-squirt"></i>
+        <h1>${name}</h1>
+      </div>
+`)};
+
+
+
+
