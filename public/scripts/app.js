02864b62013f43e9338fff4cc5cb1541186d685c
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-10 06:35:11 +0000

    move jquery event listeners related to create quiz from app.js to its own file in the scripts folder

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,44 +1,4 @@
 // Client facing scripts here
 
-const additionalOption = 
-  `<div class="modular-option-node">
-  <textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
-  <fieldset>
-    <input type="checkbox" id="correct" name="correct">
-    <span>Correct?</span>
- </div>
-   `;
 
-const additionalQuestion = 
-`<article id="question-option-container">
-<textarea id="new-quiz-question" name="question" placeholder="Enter your question" required=""></textarea>
-<div id="options-parent">
-  <div id="options-target">
-  <div class="modular-option-node">
-<textarea id="new-option" name="option" placeholder="Enter a possible answer"></textarea>
-<fieldset>
-  <input type="checkbox" id="correct" name="correct">
-  <span>Correct?</span>
-</div>
-</div>
-<button class="nother-option">+</button>
-</div>
-<div class="button-flex">
-<button class="save-button">+ Add next question</button>
-</div>
-</article>`;
-
-$(document).ready(function() { 
-  
-  $(document).on('click', '.nother-option', function(e) {
-    e.preventDefault();
-    $(this).parent().find("#options-target").append(additionalOption);
-  })
-
-  $(document).on('click', '.save-button', function(e) {
-    e.preventDefault();
-    $(this).hide();
-    $(this).closest(".questions-container").append(additionalQuestion);
-  })
-});
 
