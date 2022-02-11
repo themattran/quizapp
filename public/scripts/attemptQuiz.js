0d005d6cbd7e448204300dd8748023baa8bdebbe
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-11 06:03:12 +0000

    fix spacing in attemptQuiz file

--- public/scripts/attemptQuiz.js
+++ public/scripts/attemptQuiz.js
@@ -1,54 +1,10 @@
-let quizObj = {
-  "name": "Third quiz",
-  "questions": [
-    {
-      "question_string": "who is president in Ukraine?",
-      "options": [
-        {
-          "content": "Volodymyr Zelensky",
-          "is_correct": true
-        },
-        {
-          "content": "Joe Biden",
-          "is_correct": false
-        },
-        {
-          "content": "Vladimir Putin",
-          "is_correct": false
-        }
-      ]
-    },
-    {
-      "question_string": "who is president in the US?",
-      "options": [
-        {
-          "content": "Volodymyr Zelensky",
-          "is_correct": false
-        },
-        {
-          "content": "Donald Trump",
-          "is_correct": false
-        },
-        {
-          "content": "Joe Biden",
-          "is_correct": true
-        },
-        {
-          "content": "Vladimir Putin",
-          "is_correct": false
-        }
-      ]
-    }
-  ]
-};
-
 const attemptQuizElement = (quizObj) => {
   let quizElement = `<div id="quiz-title">${quizObj.name}</div>`
   for (let q of quizObj.questions) { 
     quizElement += 
       `<article>
         <div id="question-content">
-          ${q.question_string}
+          ${q.questionString}
         </div>
         <div id="options-container">`;
         for (let o of q.options) {
@@ -69,23 +25,13 @@ const attemptQuizElement = (quizObj) => {
  }
 
  $(document).ready(function() {
-  $("#nav_my-quizzes").click(renderQuizAttempt(attemptQuizElement(quizObj)));
- })
-
-//  <div id="quiz-title">
-//   ${quizObj.name}
-// </div>
-// <% for (let q of quizObj.questions) { %>
-//  <article>
-//    <div id="question-content">
-//      ${q.question_string}
-//    </div>
-//    <div id="options-container">
-//    <% for (let o of q.options) { %>
-//      <button id="each-option">
-//        ${o.content}
-//      </button>
-//    <% } %>
-//    </div>
-//  </article>
-//  <% } %>
+  $("#nav_my-quizzes").click(function() {
+    $.ajax({
+      method: "GET",
+      url: "/api/quizzes/3"
+    }).done((res) => {
+      console.log(res)
+      renderQuizAttempt(attemptQuizElement(res));
+    });
+  });
+ });
