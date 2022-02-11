93d0f3d48b2653e6d0713e7fa187c5fbe39aeb6e
Johannes Weber <johannes@drweber.de>
2022-02-11 11:03:05 -0800

    add scoring logic

--- public/scripts/attemptQuiz.js
+++ public/scripts/attemptQuiz.js
@@ -1,3 +1,28 @@
+/**
+ * Parse the form and score the quiz
+ * @returns object with scoring data: {totalQuestions, correctAnswers, percentage, score}
+ */
+const scoreQuiz = () => {
+  const questionContainers = $("#attempt-quiz > article");
+  const totalQuestions = questionContainers.length;
+  let correctAnswers = 0;
+  for (const questionContainer of questionContainers) {
+    const selectedOption = $(questionContainer).find('.selected')[0];
+    const answerIsCorrect = $(selectedOption).hasClass('correct');
+    if (answerIsCorrect) {
+      correctAnswers++;
+    }
+  }
+  const result = {
+    totalQuestions,
+    correctAnswers,
+    percentage: `${Math.round(correctAnswers / totalQuestions * 100)}%`,
+    score: `${correctAnswers}/${totalQuestions}`,
+  };
+  return result;
+};
+
+
 $(document).ready(function() {
   //event listener for attempt quiz link
   $(document).on('click', '.each-option', function (e) {
@@ -16,5 +41,12 @@ $(document).ready(function() {
   //   }
   //   console.log(rightAnswers);
   // });
+
+  /**
+   * Event listener for submitting/scoring
+   */
+  $("#attempt-quiz").on('click', '#submit-attempt', function() {
+    console.log(scoreQuiz());
+  });
 });
 
