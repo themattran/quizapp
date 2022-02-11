86f5a9d1d5f9b7d43a425b37b5186ea58dcf1a4e
Matthew Tran <m.tran906@gmail.com>
2022-02-11 23:12:02 +0000

    Make my quizzes view scripts

--- public/scripts/my-quizzes.js
+++ public/scripts/my-quizzes.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //event listener for attempt quiz link
-  $(document).on('click', '.attempt-link', function (e) {
+  $(document).on('click', '.my-attempt-link', function (e) {
     e.preventDefault();
     console.log($(this)[0].id);
     $.ajax({
@@ -10,6 +10,7 @@ $(document).ready(function() {
       console.log(res);
       renderQuizAttempt(attemptQuizElement(res));
       //unclear how to switch view from here
+      switchToView(".my-quizzes");
     });
   });
 
@@ -18,43 +19,48 @@ $(document).ready(function() {
         url: "/api/quizzes?limit=4&orderBy=random"
       }).done((res) => {
         console.log('res', res);
-        renderQuizzes(res);
+        renderMyQuizzers(res);
    });
 });
 
 //Function that renders quizzes and appends database questions to main container
-const renderQuizzes = function(quizzes) {
+const renderMyQuizzers = function(quizzes) {
   // $("#list-quizzes").empty();
   console.log('quizzes', quizzes);
   for (const quiz of quizzes) {
-     $('#list-quizzes').append(quizCard(quiz.name, quiz.id));
+     $('#my-quizzes').append(quizCard(quiz.name, quiz.id));
   };
 }
 
 //Function that returns quiz card element
 const quizCard = (name, id) => {
   return $(`
-<div class="quiz-card">
+<div class="my-quiz-card">
         <i class="fa-duotone fa-gun-squirt"></i>
         <h1>${name}</h1>
-        <a class="attempt-link" id="${id}">Attempt Quiz</a>
+        <a class="my-attempt-link" id="${id}">Attempt Quiz</a>
+        <a class="my-share-link" id="">Share Link</a>
+        <div class= "my-option-node">
+        <fieldset>
+             <input type="checkbox" id="correct" name="correct">
+             <span>Correct?</span>
       </div>
 `)};
 
 //Function that returns interactable quiz element
 const attemptQuizElement = (quizObj) => {
-  let quizElement = `<div id="quiz-title" class="container">${quizObj.name}</div>`
+  let myQuizElement = `<div id="my-quiz-title" class="container">${quizObj.name}</div>`
   for (let q of quizObj.questions) {
-    quizElement +=
+    myQuizElement +=
       `<article class="container">
         <div class="question-content">
           ${q.questionString}
         </div>
         <div class="options-container">`;
         for (let o of q.options) {
-          o.isCorrect ? quizElement += `<button class="each-option correct">${o.content}</button>` : quizElement += `<button class="each-option">${o.content}</button>`;
+          o.isCorrect ? myQuizElement += `<button class="each-option correct">${o.content}</button>` : myQuizElement += `<button class="each-option">${o.content}</button>`;
         }
-    quizElement +=
+    myQuizElement +=
     `</div>
     </article>`;
   }
