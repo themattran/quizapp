64ae843601d490f5fca87fcfe8efe719f83c1987
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-11 07:50:35 +0000

    add frontend logic to display chosen quizzes

--- public/scripts/attemptQuiz.js
+++ public/scripts/attemptQuiz.js
@@ -1,36 +1,3 @@
-const attemptQuizElement = (quizObj) => {
-  let quizElement = `<div id="quiz-title">${quizObj.name}</div>`
-  for (let q of quizObj.questions) { 
-    quizElement += 
-      `<article>
-        <div id="question-content">
-          ${q.questionString}
-        </div>
-        <div id="options-container">`;
-        for (let o of q.options) {
-          quizElement += 
-          `<button id="each-option">
-            ${o.content}
-          </button>`
-        }
-    quizElement += 
-    `</div>
-    </article>`;
-  }
-  return quizElement;
-  }
 
- const renderQuizAttempt = function (quiz) {
-   $("#attempt-quiz").append(quiz);
- }
 
- $(document).ready(function() {
-  $("#nav_my-quizzes").click(function() {
-    $.ajax({
-      method: "GET",
-      url: "/api/quizzes/3"
-    }).done((res) => {
-      renderQuizAttempt(attemptQuizElement(res));
-    });
-  });
- });
+
