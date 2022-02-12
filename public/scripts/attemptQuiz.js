d2c9a1d5e139a16506240d09bc344181aa20e6bf
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-02-12 04:57:28 +0000

    generate dynamic score

--- public/scripts/attemptQuiz.js
+++ public/scripts/attemptQuiz.js
@@ -33,7 +33,7 @@ const scoreQuiz = () => {
   const result = {
     totalQuestions,
     correctAnswers,
-    percentage: `${Math.round(correctAnswers / totalQuestions * 100)}%`,
+    percentage: Math.round(correctAnswers / totalQuestions * 100),
     score: `${correctAnswers}/${totalQuestions}`,
   };
   return result;
@@ -58,17 +58,32 @@ const attemptQuizElement = (quizObj) => {
   return quizElement;
 }
 
-const scoreElement = (score) => 
-`<div>
-<article class="container score-container">
-  You got 5/9 questions correct!
-  <img src="https://i.imgur.com/Sa75Uzg.gif" class="gif">
+const scoreElement = (score) => {
+  let url;
+  if (score.percentage === 100) {
+    url = "https://i.imgur.com/y4HTi3B.gif";
+  } else if (score.percentage < 100 && score.percentage > 80){
+    url = "https://c.tenor.com/x8dTXjiggP0AAAAd/itysl-bottle.gif";
+  } else if (score.percentage <= 80 && score.percentage > 60) {
+    url = "https://c.tenor.com/7ms_eNspHEEAAAAC/itysl-i-think-you-should-leave.gif";
+  } else if (score.percentage <= 60 && score.percentage > 40) {
+    url = "https://i.imgur.com/Sa75Uzg.gif";
+  } else if (score.percentage <= 40 && score.percentage > 15) {
+    url = "https://media0.giphy.com/media/YmbRfeiISGdKXguBvE/giphy.gif?cid=4d1e4f2915d7fafe553499bc77d1f7027874022646fe38e4&rid=giphy.gif";
+  } else if (score.percentage <= 15) {
+    url = "https://c.tenor.com/IRXQni1lHxgAAAAd/karl-havoc.gif";
+  }
+  return `<div>
+  <article class="container score-container">
+  You got ${score.score} questions correct!
+  <img src="${url}" class="gif">
   <div class="icons">
     <i class="fa-solid fa-link"></i>
     Share your result
   </div>
-</article>
-</div>`;
+  </article>
+  </div>`
+};
 
 //appending element returned by attemptQuizElement to the #attempt-quiz section
  const renderQuizAttempt = function (quiz) {
@@ -90,9 +105,10 @@ $(document).ready(function() {
    * Event listener for submitting/scoring
    */
   $("#attempt-quiz").on('click', '#submit-attempt', function() {
-    console.log(scoreQuiz());
+    let score = scoreQuiz();
+    console.log(score);
     $(this).hide();
-    $(this).parent().append(scoreElement);
+    $(this).parent().append(scoreElement(score));
   });
 });
 
