9ed45f54a8d4eeb0e25ffd21d32daa05e8cdefc7
Matthew Tran <m.tran906@gmail.com>
2022-02-12 01:53:02 +0000

    create my quizzes vie wwith checkbox and attempt quiz functionality

--- public/scripts/my-quizzes.js
+++ public/scripts/my-quizzes.js
@@ -1,26 +1,32 @@
+
 $(document).ready(function() {
   //event listener for attempt quiz link
   $(document).on('click', '.my-attempt-link', function (e) {
     e.preventDefault();
+    const quizId = $(this)[0].id;
+    console.log("Attempt Quiz ID", quizId);
+    switchToView('attempt-quiz', {quizId}); 
+  });
+
+  //Unlist Route checkout +
+  $(document).on('click', '#is-public', function (e) {
+    e.preventDefault();
     console.log($(this)[0].id);
     $.ajax({
-      method: "GET",
-      url: `/api/quizzes/${$(this)[0].id}`
+      method: "POST",
+      url: `/api/quizzes/33/list`
     }).done((res) => {
       console.log(res);
-      renderQuizAttempt(attemptQuizElement(res));
-      //unclear how to switch view from here
-      switchToView(".my-quizzes");
     });
   });
 
   $.ajax({
         method: "GET",
-        url: "/api/quizzes?limit=4&orderBy=random"
+        url: "/api/quizzes/myquizzes"
       }).done((res) => {
-        console.log('res', res);
         renderMyQuizzes(res);
    });
+
 });
 
 //Function that renders quizzes and appends database questions to main container
@@ -28,13 +34,13 @@ const renderMyQuizzes = function(quizzes) {
   // $("#list-quizzes").empty();
   console.log('quizzes', quizzes);
   for (const quiz of quizzes) {
-     $('#my-quizzes').append(quizCard(quiz.name, quiz.id));
+     $('#my-quizzes').append(myQuizCard(quiz.name, quiz.id));
   };
 }
 
 //Function that returns quiz card element
-const quizCard = (name, id) => {
-  return $(`
+const myQuizCard = (name, id) => {
+  return `
 <div class="my-quiz-card">
         <i class="fa-duotone fa-gun-squirt"></i>
         <h1>${name}</h1>
@@ -42,14 +48,15 @@ const quizCard = (name, id) => {
         <a class="my-share-link" id="">Share Link</a>
         <div class= "my-option-node">
         <fieldset>
-             <input type="checkbox" id="correct" name="correct">
-             <span>Correct?</span>
+             <input type="checkbox" id="is-public" name="correct">
+             <span>Make Public</span>
       </div>
-`)};
+`};
 
 //Function that returns interactable quiz element
-const attemptQuizElement = (quizObj) => {
+const myAttemptQuizElement = (quizObj) => {
   let myQuizElement = `<div id="my-quiz-title" class="container">${quizObj.name}</div>`
+  console.log('quizObj', quizObj);
   for (let q of quizObj.questions) {
     myQuizElement +=
       `<article class="container">
@@ -67,8 +74,8 @@ const attemptQuizElement = (quizObj) => {
   return quizElement;
   }
 
-//appending element returned by attemptQuizElement to the #attempt-quiz section
- const renderQuizAttempt = function (quiz) {
+//appending element returned by myAttemptQuizElement to the #attempt-quiz section
+ const myRenderQuizAttempt = function (quiz) {
    $("#attempt-quiz").empty();
    $("#attempt-quiz").append(quiz);
    $("#attempt-quiz").append('<button class="container" id="submit-attempt">Submit and show my score!</button>');
