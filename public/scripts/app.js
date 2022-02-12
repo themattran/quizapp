df8d7569628a4f955ae167950660008c083246db
Johannes Weber <johannes@drweber.de>
2022-02-12 09:21:50 -0800

    add generic copyText function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,3 +2,13 @@
 
 
 
+// Copy text to clipboard
+const copyText = function(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      console.log("Copied to clipboard: ",text);
+    })
+    .catch(err => {
+      console.log('Something went wrong', err);
+    });
+};
